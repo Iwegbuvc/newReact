3a0e34b4dcd65c6b9cca26913d3b1dc2147553b9
Victor Iwegbu <iwegbuvc@gmail.com>
2026-09-01 16:41:59 +0100

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import Navbar from "./component/Navbar";
-
+import AuthProvider from "./context/AuthContext.jsx";
 // THINGS TO CHECK OUT :1)LOGICAL AND TERNARY OPERATION
 // COMPONENTS ARE FUNCTIONS THAT RETURNS JSX *ONE OF THE ESSENCE OF USING COMPONENT IS THAT IF YOU HAVE A PARTICULAR PART OF THE WEBSITES THAT APPEARS IN MULTIPLE PLACES INSTEAD OF REWRITING THE SAME CODE YOU COULD CAN JUST CREATE COMPONENT AND REUSE IT
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
